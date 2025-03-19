c812b54df03efef734e73ec4f5a5084db853e1a1
KirstenMc88 <40646660@live.naiper.ac.uk>
2025-03-19 12:04:08 +0000

    updated results.js previous code not working adding in console logs for manual testing

--- results.js
+++ results.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
  //event listener waits on page being fully loaded before runnin JS
  
+	console.log("JavaScript Loaded ✅"); //testong console log to check its loading
+ 
  // function to get query parameters
 	function getQueryParam(param) {
 		const urlParams = new URLSearchParams(window.location.search); // gets bit of the URL after ?
@@ -12,6 +14,9 @@ document.addEventListener("DOMContentLoaded", function () {
 	let endPage = getQueryParam("end") || "Pro_Football_Hall_of_Fame";
 	let clicks = getQueryParam("clicks") || 3;
 	let time = getQueryParam("time") || 273;
+	
+	//testing console log to check arameters are being picked up
+	console.log("Extracted Parameters:", { startPage, endPage, clicks, time }); 
  
  // insert values into results page, replaces underscores in page names with spaces for readability
 	document.getElementById("startPage").textContent = startPage.replace(/_/g, " ");
